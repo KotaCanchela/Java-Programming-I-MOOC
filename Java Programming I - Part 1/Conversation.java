6198181efe5b06a88cb9f671d1b14fc750f7ec5c
KotaCanchela <dakotacanchela@gmail.com>
2020-08-01 11:30:28 -0700

    Completed conversation class

--- Java Programming I - Part 1/Conversation.java
+++ Java Programming I - Part 1/Conversation.java
@@ -1,5 +1,15 @@
 
+import java.util.Scanner;
 
 public class Conversation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Greetings how are you doing?");
+        String first_response = scanner.nextLine();
 
+        System.out.println("Oh, how interesting. Tell me more!");
+        String second_response = scanner.nextLine();
+
+        System.out.println("Thanks for sharing!");
+    }
 }
