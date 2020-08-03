b89ebf256f2bf630275376d70041819238db4b15
KotaCanchela <dakotacanchela@gmail.com>
2020-08-02 20:23:05 -0700

    Commit

---
+++ Java Programming I - Part 2/Repeating Functionality/AreWeThereYet.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+// Write a program, according to the preceding example, that asks the user
+// to input values until they input the value 4.
+public class AreWeThereYet {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scan.nextLine());
+            if (number == 4) {
+                break;
+            }
+        }
+    }
+}
