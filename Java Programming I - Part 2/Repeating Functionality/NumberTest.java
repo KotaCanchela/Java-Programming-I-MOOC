13d98f0a72116228b995c7d47e95409ffff1d9c7
KotaCanchela <dakotacanchela@gmail.com>
2020-08-02 20:38:16 -0700

    test

--- Java Programming I - Part 2/Repeating Functionality/NumberTest.java
+++ Java Programming I - Part 2/Repeating Functionality/NumberTest.java
@@ -9,7 +9,7 @@ public class NumberTest {
             }
 
             if (number < 5) {
-                continue;
+                continue; // Continue brings the program back to the beginning of the loop. i.e (number = number + 1)
             }
             System.out.print(number + " "); // It seems like this line doesn't even execute.
         }
