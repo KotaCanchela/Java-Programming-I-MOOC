314f47e8a6008eb9ddc75e93e99d14db525dcbf2
KotaCanchela <dakotacanchela@gmail.com>
2020-08-02 21:49:03 -0700

    Repeating functionality section completed

---
+++ Java Programming I - Part 2/Repeating Functionality/AverageOfPositiveNumbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+// Write a program that asks the user for input until the user inputs 0. After this,
+// the program prints the average of the positive numbers (numbers that are greater than zero).
+//If no positive number is inputted, the program prints "Cannot calculate the average"
+public class AverageOfPositiveNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numberSum = 0;
+        int numTracker = 0;
+        while (true) {
+            int input = Integer.valueOf(scan.nextLine());
+            if (input == 0) {
+                break;
+            } else if (input < 0) {
+                continue;
+            }
+            numTracker += 1;
+            numberSum += input;
+        }
+        if (numTracker > 0) {
+            double average = (double) numberSum / numTracker;
+            System.out.println(average);
+        } else if (numTracker == 0) {
+            System.out.println("Cannot calculate the average");
+        }
+    }
+}
