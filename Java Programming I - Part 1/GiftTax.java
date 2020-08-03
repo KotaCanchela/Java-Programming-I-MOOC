056730157a5a72e9033567ca28f88517508480c9
KotaCanchela <dakotacanchela@gmail.com>
2020-08-02 19:03:09 -0700

    Cleaning code

--- Java Programming I - Part 1/GiftTax.java
+++ Java Programming I - Part 1/GiftTax.java
@@ -9,15 +9,15 @@ public class GiftTax {
         if (value < 5000) {
             System.out.println("No tax!");
         } else if (value >= 5000 && value < 25_000) {
-            System.out.println("Tax: " + (100 + (value-5000)*0.08));
+            System.out.println("Tax: " + (100 + (value - 5000) * 0.08));
         } else if (value >= 25000 && value < 55_000) {
-            System.out.println("Tax: " + (1700 + (value-25000)*0.10));
+            System.out.println("Tax: " + (1700 + (value - 25000) * 0.10));
         } else if (value >= 55000 && value < 200_000) {
-            System.out.println("Tax: " + (4700 + (value-55000)*0.12));
+            System.out.println("Tax: " + (4700 + (value - 55000) * 0.12));
         } else if (value >= 200_000 && value < 1_000_000) {
-            System.out.println("Tax: " + (22_100 + (value-200_000)*0.15));
+            System.out.println("Tax: " + (22_100 + (value - 200_000) * 0.15));
         } else if (value > 1_000_000) {
-            System.out.println("Tax: " + (142_100 + (value-1_000_000)*0.17));
+            System.out.println("Tax: " + (142_100 + (value - 1_000_000) * 0.17));
         }
     }
 }
