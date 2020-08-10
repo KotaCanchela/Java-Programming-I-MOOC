e96e1d3a2590d715b4be995fd568836f4ee470b5
KotaCanchela <dakotacanchela@gmail.com>
2020-08-09 21:28:25 -0700

    Fixed film module

--- Java Programming I - Part 4/Introduction to OOP/FilmTest.java
+++ Java Programming I - Part 4/Introduction to OOP/FilmTest.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class FilmTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Film test = new Film("Jurassic Park", 18);
+
+        System.out.println(test.name() + " has an age rating of " + test.ageRating());
+
+        System.out.println("How old are you?");
+        int age = Integer.valueOf(scan.nextLine());
+
+        if (age >= test.ageRating()) {
+            System.out.println("You are old enough to watch " + test.name());
+        } else {
+            System.out.println("Unforunately you are not old enough to watch " + test.name());
+        }
+
+    }
 }
