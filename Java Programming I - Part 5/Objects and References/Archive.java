d91a63aa0fb2e6f51b7c0249b30b3636890465f7
KotaCanchela <dakotacanchela@gmail.com>
2020-08-18 19:34:52 -0700

    Archive module complete

---
+++ Java Programming I - Part 5/Objects and References/Archive.java
@@ -0,0 +1,45 @@
+public class Archive {
+    private String identifier;
+    private String name;
+
+    public Archive(String identifier, String name) {
+        // Archive constructor created with name and identifier strings
+        this.name = name;
+        this.identifier = identifier;
+    }
+
+    public String getIdentifier() {
+        return this.identifier;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public boolean equals(Object compared) {
+        // Compares if objects are in the same position
+        if (this == compared) {
+            return true;
+        }
+
+        // If object is not of type Archive, they are not equal
+        if (!(compared instanceof Archive)) {
+            return false;
+        }
+
+        // Converts the object into Archive object
+        Archive archiveObject = (Archive) compared;
+
+        // Compares name and identifier string of identifier object, returns true if they are equal
+        if (this.identifier.equals(archiveObject.identifier)
+        && this.name.equals(archiveObject.name)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override public String toString() {
+        return this.getIdentifier() +": " + this.getName();
+    }
+}
---
+++ Java Programming I - Part 5/Objects and References/ArchiveRun.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class ArchiveRun {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Archive> archives = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Identifier? (empty will stop)");
+            String identifier = scan.nextLine();
+            if (identifier.isEmpty()) {
+                break;
+            }
+
+            System.out.println("Name? (empty will stop)");
+            String name = scan.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+
+            Archive book = new Archive(identifier, name);
+            if (!archives.contains(book)) {
+                archives.add(book);
+            }
+
+        }
+
+
+
+        for (Archive archive: archives) {
+            System.out.println(archive);
+        }
+    }
+}
