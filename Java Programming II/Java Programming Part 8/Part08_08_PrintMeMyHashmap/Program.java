93e25d8b6539df48cce2689b73214c5a44b240df
KotaCanchela <dakotacanchela@gmail.com>
2020-09-09 22:45:38 -0700

    Complete

--- Java Programming II/Java Programming Part 8/Part08_08_PrintMeMyHashmap/Program.java
+++ Java Programming II/Java Programming Part 8/Part08_08_PrintMeMyHashmap/Program.java
@@ -1,2 +1,39 @@
+import java.util.HashMap;
+
 public class Program {
+    public static void main(String[] args) {
+        HashMap<String, String> hashmap = new HashMap<>();
+        hashmap.put("thank you", "merci");
+        hashmap.put("hi", "salut");
+        hashmap.put("hello", "bonjour");
+        hashmap.put("i am hungry", "j'ai faim");
+
+        printKeys(hashmap);
+        printKeysWhere(hashmap, "ll"); // expected to return "hello"
+        printValuesOfKeysWhere(hashmap, "ll"); // expected to return the value of "hello" 
+
+    }
+
+    public static void printKeys(HashMap<String, String> hashmap) {
+        // Prints all the keys in the hashmap given as a parameter
+        System.out.println(hashmap.keySet());
+    }
+
+    public static void printKeysWhere(HashMap<String, String> hashmap, String text) {
+        // Prints the keys in the hashmap given as a parameter which contain the string given as a parameter
+        for (String key: hashmap.keySet()) {
+            if (key.contains(text)) {
+                System.out.println(key);
+            }
+        }
+    }
+
+    public static void printValuesOfKeysWhere(HashMap<String, String> hashmap, String text) {
+        // Prints the values in the given hashmap which keys contain the given string
+        for (String key: hashmap.keySet()) {
+            if (key.contains(text)) {
+                System.out.println(hashmap.get(key));
+            }
+        }
+    }
 }
