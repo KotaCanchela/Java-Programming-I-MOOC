012e7aec9c025d5de6ee07ee36273b126e4f4a7f
KotaCanchela <dakotacanchela@gmail.com>
2020-09-12 15:30:38 +0100

    Refactored

--- Java Programming II/Java Programming Part 8/Part08_13_VehicleRegistry/VehicleRegistry.java
+++ Java Programming II/Java Programming Part 8/Part08_13_VehicleRegistry/VehicleRegistry.java
@@ -12,41 +12,28 @@ public class VehicleRegistry {
         // Assigns the owner it received as a parameter to a car that corresponds to the license plate received as a
         // parameter. If the license plate doesn't have an owner, the method returns true. If the license plate
         // already has an owner attached, the method returns false and does nothing
-        for (LicensePlate license: this.vehicleReg.keySet()) {
-            if (license.hashCode() == licensePlate.hashCode()) {
-                return false;
-            }
+        if (this.vehicleReg.containsKey(licensePlate)) {
+            return false;
         }
-        if (!(this.vehicleReg.containsValue(owner) && this.vehicleReg.containsKey(licensePlate))) {
-            this.vehicleReg.put(licensePlate, owner);
-            return true;
-        }
-        return false;
+
+        this.vehicleReg.put(licensePlate, owner);
+        return true;
     }
 
     public String get(LicensePlate licensePlate) {
         // Returns the owner of the car corresponding to the license plate received as a parameter.
         // If the car isn't in the registry, the method returns null
-
-        for (LicensePlate license: this.vehicleReg.keySet()) {
-            if (license.equals(licensePlate)) {
-                return this.vehicleReg.get(license);
-            }
-        }
-
-        return null;
+        return this.vehicleReg.get(licensePlate);
     }
 
     public boolean remove(LicensePlate licensePlate) {
         // Removes the license plate and attached data from the registry. The method returns true if removed successfully.
-        for (LicensePlate license: this.vehicleReg.keySet()) {
-            if (license.equals(licensePlate)) {
-                this.vehicleReg.remove(license);
-                return true;
-            }
+        if (!(this.vehicleReg.containsKey(licensePlate))) {
+            return false;
         }
 
-        return false;
+        this.vehicleReg.remove(licensePlate);
+        return true;
     }
 
     public void printLicensePlates() {
