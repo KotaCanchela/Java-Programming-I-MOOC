8abf226eedae3e72d258b1f17730df9e7453482f
KotaCanchela <dakotacanchela@gmail.com>
2020-11-03 19:46:11 +0000

    Point 3d complete

---
+++ Java Programming II/Java Programming Part 9/Part_09_02.PersonAndSubclasses/PointMain.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class PointMain {
+    public static void main(String[] args) {
+        ArrayList<Point> points = new ArrayList<>();
+        points.add(new Point(4, 8));
+        points.add(new Point(8, 12));
+        points.add(new ColorPoint(4, 8, "red"));
+        points.add(new ColorPoint(4, 82, "blue"));
+        points.add(new Point(0, 0));
+
+        for (Point p: points) {
+            System.out.println(p);
+        }
+    }
+}
